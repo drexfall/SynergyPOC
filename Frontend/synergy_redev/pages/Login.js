7c06ae8bbfd58d30754c76e65bb05326ef491c43
arnavgour <133200768+arnavgour@users.noreply.github.com>
2024-08-17 17:18:50 +0530

    package.json fix

--- Frontend/synergy_redev/pages/Login.js
+++ Frontend/synergy_redev/pages/Login.js
@@ -12,7 +12,7 @@ export default function Login() {
     const data = await response.json()
     console.log(data);
   }
- 
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
