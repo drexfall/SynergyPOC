d1379a90bf918d5e742762086d04edcfeb7b2d5f
arnavgour <133200768+arnavgour@users.noreply.github.com>
2024-08-17 16:12:20 +0530

    login page linked

---
+++ Frontend/synergy_redev/next.config.js
@@ -0,0 +1,24 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://localhost:44346/:path*', // Proxy to Backend
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
+    ];
+  },
+};
